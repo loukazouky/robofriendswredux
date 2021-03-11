dc0423d4e66ae29021f77e7773eba1d0e5fc5744
Rizky Ramadhan <rizky2cool@gmail.com>
2021-03-11 23:30:50 +0700

    Added service worker for PWA

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import 'tachyons';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import { searchRobots, requestRobots } from './reducers';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const logger = createLogger();
 const rootReducer = combineReducers({ searchRobots, requestRobots})
@@ -22,4 +23,6 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+serviceWorkerRegistration.register();
 reportWebVitals();
+
